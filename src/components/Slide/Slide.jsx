b53c12eaca7ec3bafe775dc2ec262265698a17b6
Dante_DEV <110292815+done-with-it@users.noreply.github.com>
2024-06-29 14:27:52 +0700

    Update trang home (thêm hình và 1 số thứ cho trang home)

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Slide = () => {
   return (
-    <div className='bg-red-600 text-center'>Slide</div>
-  )
-}
+    <div className="relative w-full overflow-hidden bg-black">
+      <div className="flex space-x-4 p-4">
+        <div className="w-1/4 flex-shrink-0 bg-opacity-50 bg-black p-2">
+          <img className="w-full h-auto max-w-full" src="src/assets/image/p1.png" alt="" />
+        </div>
+        <div className="w-1/4 flex-shrink-0 bg-opacity-50 bg-black p-2 mt-2 sm:mt-0">
+          <img className="w-full h-auto max-w-full" src="src/assets/image/p2.png" alt="" />
+        </div>
+        <div className="w-1/4 flex-shrink-0 bg-opacity-50 bg-black p-2 mt-2 sm:mt-0">
+          <img className="w-full h-auto max-w-full" src="src/assets/image/p3.png" alt="" />
+        </div>
+        <div className="w-1/4 flex-shrink-0 bg-opacity-50 bg-black p-2">
+          <img className="w-full h-auto max-w-full" src="src/assets/image/p1.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Slide
+export default Slide;
--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -6,9 +6,7 @@ const HomePage = () => {
     <main className="home-page">
       <Banner />
       <Slide />
-      <ProductList />
-      <ProductList />
-      <ProductList />
+      {/* <ProductList /> */}
     </main>
   );
 };
